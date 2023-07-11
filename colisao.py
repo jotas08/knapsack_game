c81c413e9d62452d5dd1769dfc6b5009c8dc2bdd
thaismajory2014 <thaismajory2014@gmail.com>
2023-07-11 09:03:01 -0300

    delete anotacoes e colisao

--- colisao.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def collision(c1, c2):
-    return (c1[0] == c2[0]) and (c1[1] == c2[1])
-
-def on_grid_random():
-    x = random.randint(0,580)
-    y = random.randint(0,580)
-    return (x//10 * 10, y//10 * 10)
